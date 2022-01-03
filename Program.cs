b1b4c5f55c2d60b72a20c900db315ea182c1ad35
Wotanut <sambpilot@gmail.com>
2022-01-03 16:33:46 +0000

    Added direction and location

--- Program.cs
+++ Program.cs
@@ -3,20 +3,52 @@
 namespace chess
 {
 
+    public enum direction
+    {
+        North,
+        NorthEast,
+        East,
+        SouthEast,
+        South,
+        SouthWest,
+        West,
+        NorthWest,
+
+    }
     public enum pieceType
+    {
+        King,
+        Queen,
+        Bishop,
+        Knight,
+        Rook,
+        Pawn
+    }
+    public enum pieceColour
+    {
+        Black,
+        White,
+    }
+
+    class location
+    {
+        public int x {get;}
+        public int y {get;}
+        override public string ToString ()
         {
-            King,
-            Queen,
-            Bishop,
-            Knight,
-            Rook,
-            Pawn
+            return Convert.ToString(Convert.ToChar(this.x + 64)) + Convert.ToString(this.y);
         }
-        public enum pieceColour
+        public location(int x, int y)
         {
-            Black,
-            White,
+            if ( x < 1 || x > 8 || y < 1 || y > 8)
+            {
+                throw new ArgumentException("Parameter out of range, both the X and Y value must be between 1 and 8 inclusive (X=" + x + "  Y=" + y + ")");
+            }
+
+            this.x = x;
+            this.y = y;
         }
+    }
     class piece
     {
         public pieceType type {get;}
@@ -76,10 +108,47 @@ namespace chess
         {
             tiles = new piece[8,8];
             // tiles[7,7] = new piece(pieceColour.White, pieceType.Knight);
-            
         }
 
-        public string display()
+        public piece getNextPiece(direction d)
+        {
+            int dx = 0;
+            int dy = 0;
+            switch (d)
+            {
+                case direction.North:
+                    dy = 1;
+                    break;
+                case direction.NorthEast:
+                    dy = 1;
+                    dx = 1;
+                    break;
+                case direction.East:
+                    dx = 1;
+                    break;
+                case direction.SouthEast:
+                    dx = 1;
+                    dy = -1;
+                    break;
+                case direction.South:
+                    dx = -1;
+                    break;
+                case direction.SouthWest:
+                    dx = -1;
+                    dy = -1;
+                    break;
+                case direction.West:
+                    dy = -1;
+                    break;
+                case direction.NorthWest:
+                    dx = -1;
+                    dy = 1;
+                    break;
+            }
+            return null;
+        }
+
+        override public string ToString()
         {
             string output = "";
 
@@ -109,8 +178,10 @@ namespace chess
         {
             piece x = new piece(pieceColour.White, pieceType.Queen);
             board chess_board = new board();
+            
 
-            Console.WriteLine(chess_board.display());
+            Console.WriteLine(chess_board);
+            Console.WriteLine(new location(1,1));
         }
     }
 }
